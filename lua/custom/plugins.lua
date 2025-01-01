8d829e7d8100696267bd6043a90108c4175e22d0
Hardik Mutha <hardikmutha2005@gmail.com>
2025-01-01 20:38:00 +0530

    Added Customs Folder

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,28 @@
+local plugins = {
+  {
+    "mhartington/formatter.nvim",
+    event = "VeryLazy",
+    opts = function ()
+      return require "custom.configs.formatter"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts={
+      ensure_installed={
+        "eslint-lsp",
+        "prettier",
+        "typescript-language-server"
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+
+  },
+}
+return plugins
